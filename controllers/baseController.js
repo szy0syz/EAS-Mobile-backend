0f66aef71ab5481ec0120a7d5b352c1b00e42176
szy0syz <szy0syz@gmail.com>
2017-05-20 21:13:25 +0800

    ^ Update Comment

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -43,31 +43,31 @@ export default class BaseController {
   // '<form action="http://127.0.0.1:8001/user/upload" enctype="multipart/form-data" method="post">'
   async uploadImg(req, type = 'default') {
     return new Promise((resolve, reject) => {
-      const form = new formidable.IncomingForm();
-      form.uploadDir = './public/img/' + type;
-      form.parse(req, async (err, fields, files) => {
+      const form = new formidable.IncomingForm(); // 实例化子类
+      form.uploadDir = './public/img/' + type; // 定义上传路径。注意：这里formidable是从node_modules来的，所以'./'所指的是整个程序的CWD。
+      form.parse(req, async (err, fields, files) => { // 转换formdata，指明callback是异步，里面有await！
         let imgId;
         try {
-          imgId = await this.getId('imgId')
+          imgId = await this.getId('imgId') // 从数据库从获取图片id流水号
         } catch (err) {
           console.log('获取图片id失败');
-          fs.unlink(files.file.path);
+          fs.unlink(files.file.path); // 如果失败解除文件链接
           reject(err);
         }
-        const imgUrl = new Date().getTime().toString() + imgId;
-        const extname = path.extname(files.upload.name);
+        const imgUrl = new Date().getTime().toString() + imgId; // 文件的命为当前时间数字串
+        const extname = path.extname(files.upload.name); // 获取文件后缀名 .jpg
         const repath = './public/img/' + type + '/' + imgUrl + extname;
         try {
-          await fs.rename(files.upload.path, repath);
-          gm(repath)
-            .resize(400, 400, '!')
+          await fs.rename(files.upload.path, repath); // 按指定格式重命名上传文件
+          gm(repath) // 缩放图片尺寸
+            .resize(400, 400, '!') // 限制大小
             .write(repath, async (err) => {
-              if (err) {
+              if (err) { // 尝试写入，报错则reject
                 console.log('改写图片尺寸失败');
                 fs.unlink(repath);
                 reject(err);
               } else {
-                resolve(repath.replace(/^\.\/public/, ''));
+                resolve(repath.replace(/^\.\/public/, '')); // 所有成功只返回图片路径，替换'/publc'
               }
             })
         } catch (err) {
