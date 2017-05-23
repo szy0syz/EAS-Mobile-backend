92c842ee863f0be06e1fae8064b1155fdca8b24b
szy0syz <szy0syz@gmail.com>
2017-05-23 12:00:03 +0800

    ^ 更新上传图片到七牛云

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -6,11 +6,15 @@ import formidable from 'formidable'
 import Ids from '../models/ids'
 import path from 'path'
 import fs from 'fs'
-import gm from 'gm'
+// import gm from 'gm'
+import qiniu from 'qiniu'
+
+qiniu.conf.ACCESS_KEY = 'OiUlP0RxLh1eN318uvFvX4AyHeRfAGOiPmnNwdGx'
+qiniu.conf.SECRET_KEY = 'Meii5goUxxczCkctM3vM3dgdQTU5r7YwOzHeIocE'
 
 export default class BaseController {
   constructor () {
-
+    this.uploadImg = this.uploadImg.bind(this)
   }
 
   async baseQuery(conditions = '', modelName = '') {
@@ -54,22 +58,33 @@ export default class BaseController {
           fs.unlink(files.upload.path); // 如果失败解除文件链接
           reject(err);
         }
-        const imgUrl = new Date().getTime().toString() + imgId; // 文件的命为当前时间数字串
-        const extname = path.extname(files.upload.name); // 获取文件后缀名 .jpg
-        const repath = './public/img/' + type + '/' + imgUrl + extname;
+        // const imgUrl = new Date().getTime().toString() + imgId; // 文件的命为当前时间数字串
+        // 图片文件名为当前时间毫秒数字串+随机数5位+图片id号
+        const imgName = (new Date().getTime() + Math.ceil(Math.random() * 10000)).toString(16) + imgId
+        const extname = path.extname(files.upload.name) // 获取文件后缀名 .jpg
+        const repath = './public/img/' + type + '/' + imgName + extname
         try {
-          await fs.rename(files.upload.path, repath); // 按指定格式重命名上传文件
-          gm(repath) // 缩放图片尺寸
-            .resize(400, 400, '!') // 限制大小
-            .write(repath, async (err) => {
-              if (err) { // 尝试写入，报错则reject
-                console.log('改写图片尺寸失败');
-                fs.unlink(repath);
-                reject(err);
-              } else {
-                resolve(repath.replace(/^\.\/public/, '')); // 所有成功只返回图片路径，替换'/publc'
-              }
-            })
+          const key = imgName + extname; // 文件全名 含后缀
+          await fs.rename(files.upload.path, repath)
+          const token = this.uptoken('project', key) // 换取服务器端tooken
+          const qiniuImg = await this.uploadFile(token.toString(), key, repath) // key是预设的文件全名，repath是后端服务器上绝对路径
+          fs.unlink(repath)
+          res.send({
+            status: 1,
+            image_path: qiniuImg // 如果七牛传成功了会返回文件外链地址
+          })
+          // await fs.rename(files.upload.path, repath); // 按指定格式重命名上传文件
+          // gm(repath) // 缩放图片尺寸
+          //   .resize(400, 400, '!') // 限制大小
+          //   .write(repath, async (err) => {
+          //     if (err) { // 尝试写入，报错则reject
+          //       console.log('改写图片尺寸失败');
+          //       fs.unlink(repath);
+          //       reject(err);
+          //     } else {
+          //       resolve(repath.replace(/^\.\/public/, '')); // 所有成功只返回图片路径，替换'/publc'
+          //     }
+          //   })
         } catch (err) {
           console.log('改写图片路径失败');
           fs.unlink(files.upload.path);
@@ -77,5 +92,24 @@ export default class BaseController {
         }
       });
     });
+  }
+
+  uptoken (bucket, key) {
+    let putPolicy = new qiniu.re.PutPolicy(bucket + ':' + key) // 仓储名:key
+    return putPolicy.token()
+  }
+
+  uploadFile (uptoken, key, localFile) {
+    return new Promise((resolve, reject) => {
+      let extra = new qiniu.io.PutExtra()
+      qiniu.io.putFile(upload, key, localFile, extra, function(err, ret) {
+        if(!err) {
+          resolve(ret.key)
+        } else {
+          console.log('图片上传至七牛云失败', err)
+          reject(err)
+        }
+      })
+    })
   }
 }
