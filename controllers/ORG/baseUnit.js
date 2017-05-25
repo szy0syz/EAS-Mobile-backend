35ec42782d0c6e5c8446a3830eddf2caa2ecf8f7
szy0syz <szy0syz@gmail.com>
2017-05-25 16:58:43 +0800

    + init baseUnit & permission Controller

---
+++ controllers/ORG/baseUnit.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// 基本组织控制器
+// 这会可以用Sequelize查询，不用原始查询了
+// 考虑此后端和SQLServer在同一内网，故可以考虑把EAS原始表查出来
+
+class BaseUnitHandle {
+  constructor () {}
+
+  
+
+}
+
+export default new BaseUnitHandle()
---
+++ controllers/permission.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// 此控制器非基于SQLServer，则不去继承BaseController
+
+class PermissionHandle {
+  constructor () {}
+
+  
+
+}
+
+export default new PermissionHandle()
