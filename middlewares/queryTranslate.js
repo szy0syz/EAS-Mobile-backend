2c300efabeaaf8c60697fcd20ce62328d7166113
szy0syz <szy0syz@gmail.com>
2017-05-27 00:45:02 +0800

    ^ 更新查询条件中间件算法

--- middlewares/queryTranslate.js
+++ middlewares/queryTranslate.js
@@ -1,9 +1,11 @@
 'use strict'
 
+import formidable from 'formidable'
+
 //查询条件翻译：将req里的查询条件转换成sql的查询条件
 class QueryTranslate {
   constructor() {
-  
+
   }
 
   translate(req, res, next) {
@@ -11,16 +13,46 @@ class QueryTranslate {
     const user = req.session.user
     if (!user) {
       res.send({
-        status:0,
+        status: 0,
         type: 'Illegal Request',
         message: '非法操作'
       }) // 这里就不next，完成请求了
     }
 
+    if (req.url == '/saleIssue') {
+      req.queryConditions = ` se.FStorageOrgUnitID IS NOT NULL AND sb.FControlUnitID = 'a06viCxSRPSu0pa/lIWabcznrtQ='  AND mgp.FMaterialGroupStandard != '非化肥' AND sb.FBaseStatus = 4  AND sb.FBizDate between N'2017-05-01' and N'2017-05-25' `
+      const form = new formidable.IncomingForm();
+      form.parse(req, async (err, fields, files) => {
+        if (err) {
+          res.send({
+            status: 0,
+            type: 'FORM_DATA_ERROR',
+            message: '表单信息错误'
+          })
+          return
+        }
+        const { queryConditions } = fields;
+        if (queryConditions) req.queryConditions = queryConditions
+      })
+    }
 
-    //过滤采购入库单
-    req.queryConditions = ` pe.FStorageOrgUnitID IS NOT NULL AND pb.FControlUnitID = 'a06viCxSRPSu0pa/lIWabcznrtQ='  AND mgp.FMaterialGroupStandard != '非化肥' AND pb.FBaseStatus = 4   AND pb.FCreateTime between N'2017-04-26' and N'2017-04-28'`;
-    next();
+    if (req.url == '/purInWarehs') {
+      req.queryConditions = ` pe.FStorageOrgUnitID IS NOT NULL AND pb.FControlUnitID = 'a06viCxSRPSu0pa/lIWabcznrtQ='  AND mgp.FMaterialGroupStandard != '非化肥' AND pb.FBaseStatus = 4   AND pb.FCreateTime between N'2017-04-01' and N'2017-04-28' `;
+      const form = new formidable.IncomingForm();
+      form.parse(req, async (err, fields, files) => {
+        if (err) {
+          res.send({
+            status: 0,
+            type: 'FORM_DATA_ERROR',
+            message: '表单信息错误'
+          })
+          return
+        }
+        const { queryConditions } = fields;
+        if (queryConditions) req.queryConditions = queryConditions
+      })
+      next();
+    }
   }
 }
 
